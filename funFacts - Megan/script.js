a6dc8d47f7be386a05608976a6682e18286f1288
Megans28 <85909045+Megans28@users.noreply.github.com>
2021-06-14 15:59:49 -0700

    uploaded fact finder for the first time

---
+++ funFacts - Megan/script.js
@@ -0,0 +1,34 @@
+//factOne
+document.querySelector('#a').f1.addEventListener('click',factOne);
+
+function factOne(){
+document.querySelector('#b').innerHTML = "<p id='turtle'><strong>The first person convicted of speeding was going eight mph.</strong> <br><br> On Jan. 28, 1896, Arnold was spotted going four times the speed limit in his 19th-century Benz. But since the speed limit at the time was just two miles per hour, that meant he was not going too fast by today's standards. The constable had to chase him down on his bicycle. <img id='t' src='images/turtle.jpg' alt='turtle crossing a finish line' ></p>";
+}
+
+//factTwo
+document.querySelector('#a').f2.addEventListener('click',factTwo);
+
+function factTwo(){
+document.querySelector('#b').innerHTML = "<p id='airplane'><strong>You lose up to 30 percent of your taste buds during flight.</strong> <br><br> This might explain why airplane food gets such a bad reputation. The elevation in an airplane can have a detrimental effect on our ability to taste things. </p>";
+}
+
+//factThree
+document.querySelector('#a').f3.addEventListener('click',factThree);
+
+function factThree(){
+document.querySelector('#b').innerHTML = "<p id='fire'><strong>The fire hydrant patent was lost in a fire.</strong> <br><br> The fire hydrant patent is credited to Frederick Graff, who was the chief engineer for Philadelphia Water Works during the early 1800s. The patent was destroyed when the patent office in Washington, D.C., burned down in 1836. </p>";
+}
+
+//factFour
+document.querySelector('#a').f4.addEventListener('click',factFour);
+
+function factFour(){
+document.querySelector('#b').innerHTML = "<p id='jelly'><strong>There is an immortal jellyfish.</strong> <br><br>When it's an adult, the Immortal Jellyfish, scientifically named Turritopsis dohrnii, can transform its cells back to its childhood state. This usually happens when it is physically harmed, sick, or even when it is starving. </p>";
+}
+
+//factFive
+document.querySelector('#a').f5.addEventListener('click',factFive);
+
+function factFive(){
+document.querySelector('#b').innerHTML = "<p id='whale'><strong>Baby blue whales grow 200 pounds per day.</strong> <br><br>Blue Whale calves grow 200 pounds per day for the first year they are alive. </p>";
+}
